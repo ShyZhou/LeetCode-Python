93d86683d8966136f4d719b6dbe35eda519d2cc6
shuaiyuan.zhou <shuaiyuan.zhou@homes.com>
2019-04-17 09:04:41 -0400

    213 - House Robber II

--- 213.py
+++ 213.py
@@ -17,8 +17,24 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3). Total amo
 """
 
 class Solution(object):
+    def rob_gain(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        second_to_last = nums[0]
+        last = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            current = max(last, second_to_last + nums[i])
+            second_to_last = last
+            last = current
+        return last
+
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.rob_gain(nums[0:len(nums)-1]), self.rob_gain(nums[1:]))
